667ea903e9a0f1787e833df93ffbf60db584e42e
Bartosz Szura <bartosz1szura@gmail.com>
2024-11-21 20:10:52 +0100

    Twoja wiadomość zatwierdzająca

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,18 @@ import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatCardModule } from '@angular/material/card'
 import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
 import { PokemonListComponent } from "./pokemon-list/pokemon-list.component";
-import { HttpClientModule } from '@angular/common/http';
+import {} from '@angular/common/http';
 import { HttpClient } from '@angular/common/http';
 import { PokemonCard } from './pokemon-card/pokemon-card.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatToolbarModule, MatCardModule, PokemonCardComponent, PokemonListComponent, HttpClientModule],
+  imports: [CommonModule, RouterOutlet, MatToolbarModule, MatCardModule, PokemonCardComponent, PokemonListComponent, 
+// TODO: `HttpClientModule` should not be imported into a component directly.
+// Please refactor the code to add `provideHttpClient()` call to the provider list in the
+// application bootstrap logic and remove the `HttpClientModule` import from this component.
+HttpClientModule],
   templateUrl: './pokemon-list/pokemon-list.component.html',
   styleUrl: './custom-theme.scss'
 })
